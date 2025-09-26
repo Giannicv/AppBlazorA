bbeebbde667b64592d0a8300b108aad135c3520c
Giannicv <sdmfranco6@gmail.com>
2025-09-26 12:07:50 -0400

    Agregar archivos de proyecto.

---
+++ AppBlazor.Entities/LibroFormCLS.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppBlazor.Entities
+{
+    public class LibroFormCLS
+    {
+        [Required (ErrorMessage ="El id es requerido")]
+        [Range(0,int.MaxValue,ErrorMessage ="El valor debe ser positivo")]
+        public int idLibro { get; set; }
+        [Required(ErrorMessage = "El titulo es requerido")]
+        [MaxLength(100, ErrorMessage ="La Longitud Maxima es 100 caracteres")]
+        public string titulo { get; set; } = null!;
+        [Required(ErrorMessage = "El resumen es requerido")]
+        [MinLength(5,ErrorMessage ="La Longitud minima es  caracteres")]
+        public string resumen { get; set; } = null!;
+
+        [Range(1,int.MaxValue,ErrorMessage ="Debe seleccionar un tipo de libro")]
+
+
+        public int idtipolibro { get; set; }
+
+
+        public byte[]? image { get; set; }
+
+        public byte[]? archivo { get; set; }
+
+        public string nombrearchivo { get; set; } = null!;
+
+    }
+}
---
+++ BlazorAppGianniCV.API/Controllers/LibroController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorAppGianniCV.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibroController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult listarLibros()
+        {
+            try
+            {
+                return Ok();
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+        [HttpGet("{idLibro}")]
+        public IActionResult recuperaLibroPorId()
+        {
+            try
+            {
+                return Ok();
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+        [HttpDelete("{idLibro}")]
+        public IActionResult eliminarLibro(int idLibro)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
